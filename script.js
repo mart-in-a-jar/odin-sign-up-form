0ea0cdc900d5f3c3d89979492261033d5075b837
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2022-04-27 11:18:51 +0200

    Add some handling of non matching passwords

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const password = document.querySelector("#password");
 const password_confirm = document.querySelector("#password-confirm");
 const error_message = document.querySelector(".password-error");
+const form = document.querySelector("form");
+const submitButton = document.querySelector('button[type="submit"]');
 
+// Check when user types in confirm password field
 password_confirm.addEventListener("input", () => {
-    if (password.value != password_confirm.value) {
+    if (password.value !== password_confirm.value) {
         [password, password_confirm].forEach(e => {
             e.classList.add("error");
         });
+        submitButton.classList.add("buttonDisabled");
         error_message.style["display"] = "block"
     } else {
         [password, password_confirm].forEach(e => {
             e.classList.remove("error");
+            submitButton.classList.remove("buttonDisabled");
         });
         error_message.style["display"] = "none"
+    }
+});
+
+// Check when user submits form
+form.addEventListener("submit", e => {
+    if (password.value !== password_confirm.value) {
+        e.preventDefault();
     }
 });
